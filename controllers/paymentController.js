b0723f9d3d9b296b45c002475b10e4da5269552d
Ajay <ajayanupam957@gmail.com>
2024-10-20 20:13:36 +0530

    fix: payment fixed through stripe

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,66 +1,67 @@
 const asyncErrorHandler = require('../middlewares/asyncErrorHandler');
-// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const paytm = require('paytmchecksum');
 const https = require('https');
 const Payment = require('../models/paymentModel');
 const ErrorHandler = require('../utils/errorHandler');
 const { v4: uuidv4 } = require('uuid');
 
-// exports.processPayment = asyncErrorHandler(async (req, res, next) => {
-//     const myPayment = await stripe.paymentIntents.create({
-//         amount: req.body.amount,
-//         currency: "inr",
-//         metadata: {
-//             company: "Flipkart",
-//         },
-//     });
-
-//     res.status(200).json({
-//         success: true,
-//         client_secret: myPayment.client_secret, 
-//     });
-// });
-
-// exports.sendStripeApiKey = asyncErrorHandler(async (req, res, next) => {
-//     res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
-// });
-
-// Process Payment
 exports.processPayment = asyncErrorHandler(async (req, res, next) => {
+    const myPayment = await stripe.paymentIntents.create({
+        amount: req.body.amount,
+        currency: "inr",
+        description: "Testing payment integration",
+        metadata: {
+            company: "Flipkart",
+        },
+    });
 
-    const { amount, email, phoneNo } = req.body;
-
-    var params = {};
-
-    /* initialize an array */
-    params["MID"] = process.env.PAYTM_MID;
-    params["WEBSITE"] = process.env.PAYTM_WEBSITE;
-    params["CHANNEL_ID"] = process.env.PAYTM_CHANNEL_ID;
-    params["INDUSTRY_TYPE_ID"] = process.env.PAYTM_INDUSTRY_TYPE;
-    params["ORDER_ID"] = "oid" + uuidv4();
-    params["CUST_ID"] = process.env.PAYTM_CUST_ID;
-    params["TXN_AMOUNT"] = JSON.stringify(amount);
-    // params["CALLBACK_URL"] = `${req.protocol}://${req.get("host")}/api/v1/callback`;
-    params["CALLBACK_URL"] = `https://${req.get("host")}/api/v1/callback`;
-    params["EMAIL"] = email;
-    params["MOBILE_NO"] = phoneNo;
-
-    let paytmChecksum = paytm.generateSignature(params, process.env.PAYTM_MERCHANT_KEY);
-    paytmChecksum.then(function (checksum) {
-
-        let paytmParams = {
-            ...params,
-            "CHECKSUMHASH": checksum,
-        };
-
-        res.status(200).json({
-            paytmParams
-        });
-
-    }).catch(function (error) {
-        console.log(error);
+    res.status(200).json({
+        success: true,
+        client_secret: myPayment.client_secret, 
     });
 });
+
+exports.sendStripeApiKey = asyncErrorHandler(async (req, res, next) => {
+    res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
+});
+
+// Process Payment
+// exports.processPayment = asyncErrorHandler(async (req, res, next) => {
+
+//     const { amount, email, phoneNo } = req.body;
+
+//     var params = {};
+
+//     /* initialize an array */
+//     params["MID"] = process.env.PAYTM_MID;
+//     params["WEBSITE"] = process.env.PAYTM_WEBSITE;
+//     params["CHANNEL_ID"] = process.env.PAYTM_CHANNEL_ID;
+//     params["INDUSTRY_TYPE_ID"] = process.env.PAYTM_INDUSTRY_TYPE;
+//     params["ORDER_ID"] = "oid" + uuidv4();
+//     params["CUST_ID"] = process.env.PAYTM_CUST_ID;
+//     params["TXN_AMOUNT"] = JSON.stringify(amount);
+//     // params["CALLBACK_URL"] = `${req.protocol}://${req.get("host")}/api/v1/callback`;
+//     params["CALLBACK_URL"] = `https://${req.get("host")}/api/v1/callback`;
+//     params["EMAIL"] = email;
+//     params["MOBILE_NO"] = phoneNo;
+
+//     let paytmChecksum = paytm.generateSignature(params, process.env.PAYTM_MERCHANT_KEY);
+//     paytmChecksum.then(function (checksum) {
+
+//         let paytmParams = {
+//             ...params,
+//             "CHECKSUMHASH": checksum,
+//         };
+
+//         res.status(200).json({
+//             paytmParams
+//         });
+
+//     }).catch(function (error) {
+//         console.log(error);
+//     });
+// });
 
 // Paytm Callback
 exports.paytmResponse = (req, res, next) => {
