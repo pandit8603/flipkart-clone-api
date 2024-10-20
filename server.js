3644d71235f192253b46744af8edbf49bf41427f
Ajay <ajayanupam957@gmail.com>
2024-10-20 20:23:18 +0530

    dev: added health api

--- server.js
+++ server.js
@@ -19,19 +19,9 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// deployment
-__dirname = path.resolve();
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    });
-} else {
-    app.get('/', (req, res) => {
-        res.send('Server is Running! 🚀');
-    });
-}
+app.get('/', (req, res) => {
+    res.send('Server is Running! 🚀');
+});
 
 const server = app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
